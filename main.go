27407b519ce65df07b8ecc636c2fad4b928d51ab
Mate Wagner <valueinrisk@gmail.com>
2018-02-05 16:23:49 +0100

    Update main.go
    comments added

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+==begin
+These are the net present value functions of a Tbond. Shuold be updated to
+be able to project it dynamically
+==end
+
 func pv(c, r, d float64, t float64) float64{
     var cv float64 = -c
     var j float64 = t-1
@@ -27,13 +32,13 @@ func pvd(c, r, d float64, t float64) float64{
 }
 
 func soy(target, c, r, d, t float64) float64{
-    spr:=0.01
+    spr:=0.01 //initial guess, change this to start from a different point
     i:=0
     for ; math.Abs(target-pv(c,r,d+spr,t))>=0.001; {
-        spr-=pv(c,r,d+spr,t)/pvd(c,r,d+spr,t)
+        spr-=pv(c,r,d+spr,t)/pvd(c,r,d+spr,t) //tolerance is now set to 0.001, change if you re looking for a more accurate value
         i+=1
         if i>3000{ 
-            fmt.Println("error")
+            fmt.Println("error") //reaching the total error number
             break }
     }
 
@@ -41,7 +46,7 @@ func soy(target, c, r, d, t float64) float64{
 }
 
 func main() {
-	//fmt.Println("Hello, 世界")
+	//soy calculates the IRR as a spread over yield of a Tbond. Cashflow is automatically populated
 	
 	z:=soy(0.0,100.0,0.03,0.02,25.0)
 	k:=pv(100.0,0.03,0.02+z,25.0)
